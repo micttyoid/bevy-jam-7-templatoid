80b36c5527b579e9bf2bf527d3f9b1b1729c84fa
Luke Yoo <w.lukeyoo@gmail.com>
2026-02-19 04:14:32 -0800

    Correct the credit

--- src/menus/credits.rs
+++ src/menus/credits.rs
@@ -39,7 +39,7 @@ fn created_by() -> impl Bundle {
 
 fn assets() -> impl Bundle {
     grid(vec![
-        ["Ducky sprite", "CC0 by Caz Creates Games"],
+        ["Character sprite", "by Eris Esra"],
         ["Button SFX", "CC0 by Jaszunio15"],
         ["Music", "CC BY 3.0 by Kevin MacLeod"],
         [
